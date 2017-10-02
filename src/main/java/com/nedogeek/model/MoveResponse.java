72663360691a8cc9c540005448dbb5cb13d40038
Siarhei Yerashevich <v-syerashevich@expedia.com>
2017-10-03 00:29:53 +0300

    Add static move responses

--- src/main/java/com/nedogeek/model/MoveResponse.java
+++ src/main/java/com/nedogeek/model/MoveResponse.java
@@ -2,16 +2,16 @@ package com.nedogeek.model;
 
 public class MoveResponse {
 
+    public static final MoveResponse CHECK_MOVE_RESPONSE = new MoveResponse(Commands.Call);
+    public static final MoveResponse CALL_MOVE_RESPONSE = new MoveResponse(Commands.Call);
+    public static final MoveResponse RAISE_MOVE_RESPONSE = new MoveResponse(Commands.Rise);
+    public static final MoveResponse ALL_IN_MOVE_RESPONSE = new MoveResponse(Commands.AllIn);
+
     private Commands command;
     private Integer raiseAmount;
 
-    public MoveResponse(Commands command) {
+    private MoveResponse(Commands command) {
         this.command = command;
-    }
-
-    public MoveResponse(Commands command, Integer raiseAmount) {
-        this(command);
-        this.raiseAmount = raiseAmount;
     }
 
     public Commands getCommand() {
--- src/main/java/com/nedogeek/strategy/preflop/CallPreFlopStrategy.java
+++ src/main/java/com/nedogeek/strategy/preflop/CallPreFlopStrategy.java
@@ -1,6 +1,5 @@
 package com.nedogeek.strategy.preflop;
 
-import com.nedogeek.model.Commands;
 import com.nedogeek.model.MoveResponse;
 import com.nedogeek.strategy.Strategy;
 import com.nedogeek.util.MoveDataAnalyzer;
@@ -10,14 +9,16 @@ public class CallPreFlopStrategy implements Strategy {
     private double raiseProbabilityLimit = 0.9;
     private double callProbabilityLimit = 0.8;
 
-    @Override public MoveResponse evaluateResponse() {
+    @Override
+    public MoveResponse evaluateResponse() {
         double winProbability = MoveDataAnalyzer.calculateHandWinProbability();
         if (winProbability >= raiseProbabilityLimit) {
-            return new MoveResponse(Commands.Rise);
+            MoveResponse.RAISE_MOVE_RESPONSE.setRaiseAmount(null);
+            return MoveResponse.RAISE_MOVE_RESPONSE;
         } else if (winProbability >= callProbabilityLimit) {
-            return new MoveResponse(Commands.Call);
+            return MoveResponse.CALL_MOVE_RESPONSE;
         } else {
-            return new MoveResponse(Commands.Check);
+            return MoveResponse.CHECK_MOVE_RESPONSE;
         }
     }
 }
